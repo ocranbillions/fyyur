2e1aa42a4a74df348927981bfd35e5ec60a48ab0
Samuel Ocran <sammiestt@gmail.com>
2019-10-15 16:25:23 +0100

    create venue Edit venue

--- utils.py
+++ utils.py
@@ -7,3 +7,22 @@ def validate_new_artist(artist):
     return False
   else:
     return True
+
+def validate_new_venue(venue):
+  if (venue['name'] == "" or venue['city'] == "" or
+      venue['state'] == "" or venue['phone'] == "" or 
+      len(venue['genres']) == 0 or venue['facebook_link'] == "" or 
+      venue['image_link'] == "" or venue['address'] == "" 
+    ):
+    return False
+  else:
+    return True
+
+def validate_edit_venue(venue):
+  if (venue['name'] == "" or venue['phone'] == "" or 
+      len(venue['genres']) == 0 or venue['facebook_link'] == "" or 
+      venue['image_link'] == "" or venue['address'] == "" 
+    ):
+    return False
+  else:
+    return True
